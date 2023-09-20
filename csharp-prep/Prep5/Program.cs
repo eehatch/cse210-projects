67ec7f5827aab7e0936ac6c51554563909e33663
Emily Hatch <hat19006@byui.edu>
2023-09-20 20:41:37 +0800

    Added User input and output

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string userName = GetUserName();
+        int userNumber = AddNumbers();
+
+        int squareNumber = SquareNumber(userNumber);
+
+        DisplayPersonalMessage(userName, squareNumber);
     }
+    
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int AddNumbers()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayPersonalMessage(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+      
 }
