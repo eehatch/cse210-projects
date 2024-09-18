fd5f0215f6850c6669cc52b33aefba8498c9b6d5
eehatch <hat19006@byui.edu>
2024-09-18 14:52:10 -0600

    stated my if then statement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("greater than y");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("greater than z");
+        }
+        else
+        {
+            Console.WriteLine("less than both");
+        }
     }
 }
