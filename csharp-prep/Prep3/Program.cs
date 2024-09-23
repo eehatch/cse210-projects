40eb6c05c55d0ead6406a72fa6fa44c1b2bc3ae5
eehatch <hat19006@byui.edu>
2024-09-23 14:35:25 -0600

    prep3 part 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the Magic Number? ");
+        string input =  Console.ReadLine();
+        int magicNum = int.Parse(input);
+
+        Console.Write("What is your guess? ");
+        string student = Console.ReadLine();
+        int guess = int.Parse(student);
+
+        if (guess == magicNum)
+        {
+            Console.WriteLine("You Guessed it!");
+        }
+        else if (guess > magicNum)
+        {
+            Console.WriteLine("Lower!");
+        }
+        else if (guess < magicNum)
+        {
+            Console.WriteLine("Higher!");
+        }
+     
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        for (int i = 1; i <= 10; i +=1)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
