c2c2c488216ee2a0574a9cb8592176534102ed43
eehatch <hat19006@byui.edu>
2024-09-24 16:04:09 -0600

    one to ten

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,9 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-        for (int i = 1; i <= 10; i +=1)
-        {
-            Console.WriteLine(i);
-        }
     }
 }
