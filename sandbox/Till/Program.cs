e7be88f8bac5e53576084bc019b1cf7febb5e9c4
eehatch <hat19006@byui.edu>
2024-10-16 14:03:26 -0600

    first part of the prepare3

---
+++ sandbox/Till/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Till;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Cash Register!");
+
+        Bin myBin = new("quarters", 40, (float)0.25);
+        myBin.ModifyAmount(+6);
+        Console.WriteLine(myBin.TotalValue());
+
+    }
+}
