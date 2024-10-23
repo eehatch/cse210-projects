59d96959f3b55735a094d182f227253eac6231c7
eehatch <hat19006@byui.edu>
2024-10-23 17:13:41 -0600

    about to mess up the reference class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,27 +1,30 @@
 using System;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Scripture Memorizer!!"); Thread.Sleep(1000); //pause 1 second
+        Console.WriteLine("Scripture Memorizer!!"); 
+        Thread.Sleep(1000); //pause 1 second
 
-        Scripture s2 = new Scripture("Adam fell that men might be; and men are, that they might have joy.", "1 Nephi 2:25");
-        s2.Display(); Console.WriteLine();
+        Reference r2 = new Reference("1 Nephi", 2, 25);
+        Scripture s2 = new Scripture("Adam fell that men might be; and men are, that they might have joy.", "1 Nephi 2:25" );
+        s2.Display(); 
+        Console.WriteLine();
 
-        Console.Read();
-        Console.Clear();
-        Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+        //Console.Read();
+        // Console.Clear();
         while (true)
         {   
-            string input = Console.ReadLine();
-            if (input.ToLower() != "quit" )
-            s2.Display();
             Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
-            if(input.ToLower() == "quit")
-        {
-            return;
-        }
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit" )
+            {
+                break;
+            }
+            s2.Display();
+            break;
         }
     }
 }
